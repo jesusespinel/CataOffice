0d1e9050c7ecc4f77de9121e043355134c23062b
JuanSAuzaO <sebastianauza@hotmail.com>
2023-04-04 10:09:59 -0500

    ready to push changes, currently working in Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,24 +3,30 @@ import '../style-sheets/contact.css';
 const Contact = () => {
   return (
   <div className='contact_c'>
+    <div id="overlay">
+      <form id="myform">
+        <h1>Contáctanos</h1>
+        <div className="custom_f">
+          <label htmlFor="name">Name</label>
+          <input id="name" type="text" placeholder="Enter your name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" placeholder="Enter your email" />
+        </div>
+        <div>
+          <label htmlFor="message">Message</label>
+          <input id="textarea" type="textarea" placeholder="Enter your message" />
+        </div>
+        <div>
+          <button type="submit">Send</button>
+        </div>
+      </form>
 
-    <div id="overlay"></div>
-
-    <form id="myform">
-      <label htmlFor="name">Name</label>
-      <input id="name" type="text" placeholder="Enter your name" />
-      <label htmlFor="email">Email</label>
-      <input id="email" type="email" placeholder="Enter your email" />
-      <label htmlFor="message">Message</label>
-      <input id="textarea" type="textarea" placeholder="Enter your message" />
-      <button type="submit">Send</button>
-    </form>
-
-    <div className="icon-container">
-      <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-whatsapp-circle-512.png" alt="whatsapp icon" />
-      <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png" alt="instagram icon" />
-      <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png" alt="linkedin icon" />
-      <img src="https://cdn1.iconfinder.com/data/icons/geo-points-1/154/address-pointer-map-navigation-location-512.png" alt="location icon" />
+      <div className="icon-container">
+        <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png" alt="instagram icon" />
+        <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png" alt="linkedin icon" />
+      </div>
     </div>
   </div>
   )
