88e437e23f75ef82c27c3052e0ef8537c981cb4c
JuanSAuzaO <sebastianauza@hotmail.com>
2023-04-07 14:05:08 -0500

    accordion component

---
+++ src/utils/content.js
@@ -0,0 +1,19 @@
+export  const accordionData = [
+    {
+      title: 'Misión',
+      content: `Construir espacios de trabajo dinámicos y diferentes,apoyándonos en conceptos modernos donde prima el bienestar y la naturaleza logrando así una mejor interacción entre ambiente y usuario.`
+    },
+    {
+      title: 'Visión',
+      content: `Consolidarnos en el mercado como una empresa caracterizada por superar las expectativas de cada uno de nuestros clientes, por calidad,cumplimiento,puntualidad,innovación y procesos simples.`
+    },
+    {
+      title: 'Valores',
+      content: `Comulgamos con la idea de que todo parte desde el amor por lo que hacemos, por eso nos define:
+      La honestidad en cada parte del proceso.
+      La responsabilidad con aliados y colaboradores.
+      La pasión por lo que hacemos y creemos.
+      El compromiso en todos los niveles.
+      El respeto por cada una de las personas`
+    }
+  ];
