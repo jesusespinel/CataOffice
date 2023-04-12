ab8210063a5e4836dfc97f7831b53baeed414a5f
JuanSAuzaO <sebastianauza@hotmail.com>
2023-04-12 17:57:51 -0500

    footer responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,20 +6,25 @@ import Mujer from '../assets/mujer.jpeg';
 import Logo2 from '../assets/logo2-alt.png';
 
 const Hero = () => {
+
+    const handleNavigation = (e, id) => {
+        e.preventDefault();
+        document.getElementById(id).scrollIntoView({ behavior: "smooth" });
+      };
  
     return(
         <>
             <div id="home" className="carrousel">
                 <img src={Mujer} alt= "mujer" id="mujer"/>
             </div>
-            <div className="background">
-                <div className="logo-container">
-                <div className="logo">
-                    <img src ={CataOffice} alt= "Logo" id="cat" />
-                </div>
-            
-                <button>Escríbenos</button>
-            
+                <div className="background">
+                    <div className="logo-container">
+                        <div className="logo">
+                            <img src ={CataOffice} alt= "Logo" id="cat" />
+                </div>                 
+                    <button id="home-button" onClick={(e) => handleNavigation(e, "contact")}>
+                        Escríbenos
+                    </button>
                 </div>
                 <div className="logo-container2">
                     <img src={Logo2}/>
